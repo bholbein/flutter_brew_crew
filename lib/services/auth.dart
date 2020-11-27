ccf0fc7cbad1c8d6e5f9995bbf4537aafe35fefd
Bernd Holbein <bernd.holbein@googlemail.com>
2020-11-27 21:59:39 +0100

    Created own UserObj

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,21 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create user obj based on FirebaseUser
+
+  UserObj _userFromFirebaseUser(User user) {
+    return user != null ? UserObj(uid: user.uid) : null;
+  }
 
   // sign in anonymously
   Future signInAnon() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
       User user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
