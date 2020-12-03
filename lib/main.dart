cb19cb6f0e4dd142ae53277b51f3321cb9f9e360
Bernd Holbein <bernd.holbein@googlemail.com>
2020-12-03 22:34:43 +0100

    implemented forms for login and registration

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,12 +14,16 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: analytics);
   @override
   Widget build(BuildContext context) {
     return StreamProvider<UserObj>.value(
       value: AuthService().user,
       child: MaterialApp(
         home: Wrapper(),
+        navigatorObservers: [FirebaseAnalyticsObserver(analytics: analytics)],
       ),
     );
   }
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,5 +1,7 @@
 import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 
 class SignIn extends StatefulWidget {
   @override
