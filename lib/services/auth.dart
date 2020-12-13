9f1e66146f37a6d5d3903e661e54ecdd9db4fb93
Bernd Holbein <bernd.holbein@googlemail.com>
2020-12-13 15:33:43 +0100

    Create new database entry after user registered

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,5 +1,6 @@
 import 'package:brew_crew/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:brew_crew/services/database.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -11,7 +12,6 @@ class AuthService {
   }
 
   // auth change user stream
-
   Stream<UserObj> get user {
     return _auth.authStateChanges().map(_userFromFirebaseUser);
   }
@@ -50,6 +50,9 @@ class AuthService {
       UserCredential result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       User user = result.user;
+      // create a new document for the user with the uid once it has been generated (await)
+      await DatabaseService(uid: user.uid)
+          .updateUserData('0', 'new crew member', 100);
       return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,7 +1,21 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
-  // collection reference
+  final String uid;
+
+  DatabaseService({this.uid});
+
+  // create collection reference to Firebase Firestore
   final CollectionReference brewCollection =
       FirebaseFirestore.instance.collection('brews');
+
+  // method to udpate UserData given several input. Firestore will
+  // create new documents if nothing has been created for this uid
+  Future updateUserData(String sugars, String name, int strength) async {
+    return await brewCollection.doc(uid).set({
+      'sugars': sugars,
+      'name': name,
+      'strength': strength,
+    });
+  }
 }
