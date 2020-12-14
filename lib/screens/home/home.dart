dc024d61a4c3bbac3dcd67f89f49ab439fd00b36
Bernd Holbein <bernd.holbein@googlemail.com>
2020-12-14 21:54:51 +0100

    Implemented Settings Option and Bottom Sheet

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -10,6 +10,18 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //create function insite builder to be able to access context
+    void _showSettingsPanel() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+              child: Text('bottom sheet'),
+            );
+          });
+    }
+
     return StreamProvider<List<Brew>>.value(
       value: DatabaseService().brews,
       child: Scaffold(
@@ -25,6 +37,11 @@ class Home extends StatelessWidget {
                 onPressed: () async {
                   await _auth.signOut();
                 }),
+            FlatButton.icon(
+              icon: Icon(Icons.settings),
+              label: Text('Settings'),
+              onPressed: () => _showSettingsPanel(),
+            ),
           ],
         ),
         body: BrewList(),
