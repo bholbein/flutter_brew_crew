f899569575e441d81fa184255a37cce71c1f7212
Bernd Holbein <bernd.holbein@googlemail.com>
2020-12-14 22:25:22 +0100

    Implemented several form elements for settings

---
+++ lib/screens/home/settings_form.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:brew_crew/shared/constants.dart';
+
+class SettingsForm extends StatefulWidget {
+  @override
+  _SettingsFormState createState() => _SettingsFormState();
+}
+
+class _SettingsFormState extends State<SettingsForm> {
+  final _formKey = GlobalKey<FormState>();
+  final List<String> sugars = ['0', '1', '2', '3', '4'];
+
+  // form values
+  String _currentName;
+  String _currentSugars;
+  int _currentStrength = 100;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          Text(
+            'Update your brew settings',
+            style: TextStyle(fontSize: 18.0),
+          ),
+          SizedBox(height: 20.0),
+          TextFormField(
+            decoration: textInputDecoration,
+            validator: (val) => val.isEmpty ? 'Please enter a name' : null,
+            onChanged: (val) => setState(() => _currentName = val),
+          ),
+          SizedBox(height: 20.0),
+          DropdownButtonFormField(
+            value: _currentSugars ?? '0',
+            items: sugars.map((sugar) {
+              return DropdownMenuItem(
+                value: sugar,
+                child: Text('$sugar sugars'),
+              );
+            }).toList(),
+            onChanged: (val) => setState(() => _currentSugars = val),
+          ),
+          Slider(
+            value: _currentStrength.toDouble(),
+            min: 100,
+            max: 800,
+            divisions: 7,
+            label: _currentStrength.round().toString(),
+            onChanged: (val) => setState(() => _currentStrength = val.toInt()),
+          ),
+          RaisedButton(
+            color: Colors.pink[400],
+            child: Text(
+              'Update',
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () async {
+              print(_currentName);
+              print(_currentSugars);
+              print(_currentStrength);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
